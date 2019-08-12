c866a3ab58bab8278f881d5055797fd9aab215b8
Daniel Field <dannyf506@googlemail.com>
2019-08-12 22:24:38 +0100

    Added Ticket categories model

--- tickets/admin.py
+++ tickets/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from .models import Ticket, TicketStatus
+from .models import Ticket, TicketStatus, Category
 
 # Register your models here.
-admin.site.register(Ticket)
 admin.site.register(TicketStatus)
+admin.site.register(Category)
+admin.site.register(Ticket)
--- tickets/models.py
+++ tickets/models.py
@@ -8,7 +8,7 @@ class TicketStatus(models.Model):
     This is the ticket status choices
     """
     ticket_status = models.CharField(
-        max_length=11,
+        max_length=6,
         unique=True,
         choices=(("Open", "Open"),("Closed", "Closed")))
     class Meta:
@@ -16,7 +16,23 @@ class TicketStatus(models.Model):
         verbose_name_plural = ("Ticket Status")
 
     def __str__(self):
-        return self.ticket_status    
+        return self.ticket_status
+
+class Category(models.Model):
+    """
+    This is the ticket category choices
+    """
+    category = models.CharField(
+        max_length=7,
+        unique=True,
+        choices=(("Bug", "Bug"),("Feature", "Feature")))
+
+    class Meta:
+        verbose_name = ("Category")
+        verbose_name_plural = ("Categories")    
+
+    def __str__(self):
+        return self.category         
 
 
 class Ticket(models.Model):
@@ -24,7 +40,7 @@ class Ticket(models.Model):
     This is a ticket
     """
     title = models.CharField(max_length=200)
-    category = models.CharField(max_length=200)
+    category = models.ForeignKey(Category, null=True)
     details = models.TextField()
     ticket_status = models.ForeignKey(TicketStatus, null=True)
     date_created = models.DateTimeField(blank=False, null=False, auto_now_add=True)
