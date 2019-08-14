c6f63ad06a5d0a86050b39f17083fc83d1e56cab
Daniel Field <dannyf506@googlemail.com>
2019-08-14 12:56:58 +0100

    Added create or edit ticket functionality

--- tickets/urls.py
+++ tickets/urls.py
@@ -1,9 +1,11 @@
 from django.conf.urls import url
-from tickets.views import full_ticket, upvote
+from .views import full_ticket, upvote, create_or_edit_ticket
 
 
 
 urlpatterns = [
     url(r'^(?P<pk>\d+)/$', full_ticket, name='full_ticket'),
     url(r'^upvote/(?P<pk>\d+)/$', upvote, name='upvote'),
+    url(r'^new/$', create_or_edit_ticket, name='new_post'),
+    url(r'^(?P<pk>\d+)/edit/$', create_or_edit_ticket, name='edit_post'),
 ]
--- tickets/views.py
+++ tickets/views.py
@@ -1,19 +1,21 @@
 from django.shortcuts import render, get_object_or_404, redirect, reverse
 from django.utils import timezone
 from .models import Ticket
+from .forms import TicketForm
 # Create your views here.
 
 
 def get_tickets(request):
-    """This renders the index page and displays all tickets"""
-
+    """
+    This renders the index page and displays all tickets.
+    """
     tickets = Ticket.objects.filter(published_date__lte=timezone.now()).order_by('-published_date')
     return render(request, "index.html", {'tickets': tickets})
 
 def full_ticket(request, pk):
     """
     This returns a single ticket based on the ticket ID and
-    displays it in fullticket.html
+    displays it in fullticket.html.
     """
     ticket = get_object_or_404(Ticket, pk=pk)
     return render(request, "fullticket.html", {'ticket': ticket})
@@ -27,5 +29,21 @@ def upvote(request, pk):
     ticket = get_object_or_404(Ticket, pk=pk)
     ticket.upvotes += 1
     ticket.save()
-    
+
     return render(request, "fullticket.html", {'ticket': ticket})
+
+
+def create_or_edit_ticket(request, pk=None):
+    """
+    This Creates a new ticket or edits an existing ticket
+    if the is a ticket ID.
+    """
+    ticket = get_object_or_404(Ticket, pk=pk) if pk else None
+    if request.method == "POST":
+        form = TicketForm(request.POST, instance=ticket)
+        if form.is_valid():
+            ticket = form.save()
+            return redirect(full_ticket, ticket.pk)
+    else:
+        form = TicketForm(instance=ticket)
+    return render(request, 'ticketform.html', {'form': form})    
