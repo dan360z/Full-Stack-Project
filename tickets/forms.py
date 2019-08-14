6a08e6cabadf5570da60e831cd2ed1d08af01fb9
Daniel Field <dannyf506@googlemail.com>
2019-08-14 21:27:44 +0100

    Set max ammount to 999 for donations

--- tickets/forms.py
+++ tickets/forms.py
@@ -8,7 +8,7 @@ class TicketForm(forms.ModelForm):
         fields = ('title', 'email', 'details', 'ticket_status')
 
 class MakePaymentForm(forms.Form):
-    donation = forms.IntegerField(label="Donation Amount", required=False, min_value=5, widget=forms.NumberInput(attrs={'placeholder': 'Minimum donation: £5'}))
+    donation = forms.IntegerField(label="Donation Amount", required=False, min_value=5, max_value=999, widget=forms.NumberInput(attrs={'placeholder': 'Minimum donation: £5'}))
     credit_card_number = forms.CharField(label='Credit card number', required=False, widget=forms.TextInput(attrs={'placeholder': '**** **** **** ****'}))
     cvv = forms.CharField(label='Security code (CVV)', required=False, widget=forms.TextInput(attrs={'placeholder': 'CVV'}))
     exipry_date = forms.CharField(label='Expiry date', required=False, widget=forms.TextInput(attrs={'placeholder': 'MM/YY'}))
